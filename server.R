e3e1ead884bfa2e4eadd618c227fdb77c0ad0bdc
Drew Jordan <drjorda2@gmail.com>
2015-12-11 09:48:51 -0500

    fully functioning Shiny app

--- server.R
+++ server.R
@@ -13,7 +13,7 @@ shinyServer(function(input, output, session) {
   
   observe(
     {
-      updateSliderInput(session,"max_size", min = input$min_size)
+      updateSliderInput(session,"max_size", min = input$min_size + 10)
       
       if(input$clear > 0)
       {
@@ -44,6 +44,18 @@ shinyServer(function(input, output, session) {
         read.csv(inFile$datapath, header=input$header, sep=input$sep, quote=input$quote)
       }
     })
+  
+  basket_min = reactive(
+    {
+      input$min_size
+    }
+  )
+  
+  basket_max = reactive(
+    {
+      input$max_size
+    }
+  )
   
   departments = reactive(
     {
@@ -179,7 +191,46 @@ shinyServer(function(input, output, session) {
       
       #Top20 <- head(longData[order(longData$value,decreasing = TRUE),],n=20)
       #Top20
+    }else if(input$plotSelect == "bargraph1")
+    {
+      test = table()
+      
+      visit_grouped = group_by(test, VisitNumber)
+      size_trip = summarise(visit_grouped, n_items = sum(ScanCount))
+      
+      weekday = test[,c("VisitNumber", "Weekday")] %>%
+        unique()
+      weekday_joined = left_join(x = size_trip, y = weekday, 
+                                 by = "VisitNumber")
+      
+      # what the person can change, min and max of basket 
+      min = basket_min()
+      max = basket_max()
+      
+      # proportion that are within a certain size
+      big_baskets = filter(weekday_joined, n_items > min & n_items < max)
+      grouped = group_by(weekday_joined, Weekday)
+      grouped_big = group_by(big_baskets, Weekday)
+      
+      total_df = summarise(grouped, total = n())
+      big_df = summarise(grouped_big, big = n())
+      joined = left_join(total_df, big_df, by = "Weekday")
+      
+      
+      #proportion of basket size, which changes based on min and max
+      percent = mutate(joined, prop = big/total)
+      
+      percent = arrange(percent, Weekday)
+      arranged = rbind(percent[2,], percent[6,], percent[7,],
+                       percent[5,], percent[1,], percent[3,], percent[4,])
+      
+      # plot proportion, which can change 
+      barplot(arranged$prop, names.arg = c("Mon", "Tues", "Wed",
+                                           "Thurs", "Fri", "Sat", "Sun")) 
     }
+      
+    
+    
     }
   )
 })
