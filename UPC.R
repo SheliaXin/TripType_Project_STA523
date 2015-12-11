74475315c47cc0eeee8713dd2fc18f9198b57583
Arpita <am544@duke.edu>
2015-12-11 16:21:50 -0500

    added comments

--- UPC.R
+++ UPC.R
@@ -1,20 +1,28 @@
+#looking at UPC
+
 library(stringr)
 library(stringi)
 
+#reading in the training data
 data = read.csv("train.csv")
 
+#extracting the Upc column
 upc_vec = data$Upc
 head(upc_vec)
 length(upc_vec)
 
 apply(as.matrix(upc_vec),1, str_length)
 
+#initialising length of UPC vector with NAs
 upc_len = rep(NA, length(upc_vec))
+
+#length vector
 for(i in 1:length(upc_vec))
 {
     upc_len[i] = str_length(upc_vec[i])
 }
 
+#finding the number of UPC codes for each of the lengths
 upc_len_num = rep(0, 12)
 for(j in 1:12)
 {
@@ -30,10 +38,14 @@ for(j in 1:12)
   }
 }
 
+#initialising vectors for manufacturer code, family code, value code, and coupon
 upc_man = rep(NA, length(upc_vec))
 upc_fam =  rep(NA, length(upc_vec))
 upc_val =  rep(NA, length(upc_vec))
 upc_cou = rep(NA, length(upc_vec))
+
+#filling in the vectors for manufacturer code, family code, value code and coupon
+#depending on the length of the UPC code
 for(k in 1:length(upc_vec))
 {
   if(is.na(upc_vec[k]))
@@ -62,6 +74,7 @@ for(k in 1:length(upc_vec))
     }
 }
 
+#adding in the manufacturer code, family code, value code, and coupon columns to the data
 data_cleaned_upc = data
 data_cleaned_upc$manuf_cod = upc_man
 data_cleaned_upc$fam_cod = upc_fam
